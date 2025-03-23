f83adf6b84e2386196aae1bef06a609498ae54a5
mosaadahmed2 <mosaadahmed2@gmail.com>
2025-03-23 16:51:58 -0400

    added all years data to the heatmap

--- js/quakeHeatmap.js
+++ js/quakeHeatmap.js
@@ -1,16 +1,26 @@
-// Load earthquake data
-d3.csv('data/2024-2025.csv').then(data => {
-    data.forEach(d => {
-        d.date = new Date(d.time); // Assume 'time' is your date field
+// Load and combine multiple datasets
+Promise.all([
+    d3.csv('data/2023_06-2022_10.csv'),
+    d3.csv('data/2022_10-2022_01.csv'),
+    d3.csv('data/2024_02-2023_06.csv'),
+    d3.csv('data/2025quake.csv'),
+    d3.csv('data/2024-2025.csv')
+]).then(datasets => {
+    // Combine all datasets into one array
+    const combinedData = datasets.flat();
+
+    // Standardize date format
+    combinedData.forEach(d => {
+        d.date = new Date(d.time); // Ensure 'time' is the date field in each dataset
     });
 
-    // Initial Chart Rendering
-    renderHeatmap(data, 'month');
+    // Initial chart rendering
+    renderHeatmap(combinedData, 'month');
 
     // Dropdown control for binning interval
     d3.select('#binning-select').on('change', function () {
         const selectedBin = d3.select(this).property('value');
-        renderHeatmap(data, selectedBin);
+        renderHeatmap(combinedData, selectedBin);
     });
 });
 
@@ -19,7 +29,7 @@ function renderHeatmap(data, binningInterval) {
     const svg = d3.select("#heatmap");
     svg.selectAll("*").remove(); // Clear previous chart
 
-    const margin = { top: 40, right: 40, bottom: 60, left: 60 },
+    const margin = { top: 40, right: 40, bottom: 100, left: 60 },
           width = 800 - margin.left - margin.right,
           height = 400 - margin.top - margin.bottom;
 
@@ -32,7 +42,11 @@ function renderHeatmap(data, binningInterval) {
     // Time binning logic
     const binnedData = d3.rollups(
         data,
-        v => v.length, // Count earthquakes in each bin
+        v => ({
+            count: v.length,
+            minMagnitude: d3.min(v, d => +d.mag), 
+            maxMagnitude: d3.max(v, d => +d.mag)
+        }),
         d => d3.timeFormat(getTimeFormat(binningInterval))(d.date)
     );
 
@@ -42,14 +56,14 @@ function renderHeatmap(data, binningInterval) {
         .padding(0.1);
 
     const yScale = d3.scaleLinear()
-        .domain([0, d3.max(binnedData, d => d[1])])
+        .domain([0, d3.max(binnedData, d => d[1].count)])
         .nice()
         .range([height, 0]);
 
     const colorScale = d3.scaleSequential(d3.interpolateYlOrRd)
-        .domain([0, d3.max(binnedData, d => d[1])]);
+        .domain([0, d3.max(binnedData, d => d[1].count)]);
 
-    // Draw Heatmap Squares
+    // Draw Heatmap Cells
     chart.selectAll(".cell")
         .data(binnedData)
         .join("rect")
@@ -58,23 +72,30 @@ function renderHeatmap(data, binningInterval) {
         .attr("y", 0)
         .attr("width", xScale.bandwidth())
         .attr("height", height)
-        .attr("fill", d => colorScale(d[1]))
-        .on("mouseover", (event, d) => {
-            d3.select("#HeatMap-tooltip")
+        .attr("fill", d => colorScale(d[1].count))
+        .on("mouseover", function (event, d) {
+            d3.select("#tooltip")
                 .style("display", "block")
                 .html(`
-                    <strong>${d[0]}</strong> <br>
-                    Earthquakes: ${d[1]}
+                    <strong>${d[0]}</strong><br>
+                    Earthquakes: ${d[1].count} <br>
+                    Min Magnitude: ${d[1].minMagnitude.toFixed(2)}<br>
+                    Max Magnitude: ${d[1].maxMagnitude.toFixed(2)}
                 `)
                 .style("left", (event.pageX + 10) + "px")
-                .style("top", (event.pageY + 10) + "px");
+                .style("top", (event.pageY - 40) + "px"); // Correct position logic
         })
-        .on("mouseout", () => d3.select("#HeatMap-tooltip").style("display", "none"));
+        .on("mousemove", function(event) {
+            d3.select("#tooltip")
+                .style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY - 40) + "px"); // Dynamic position update
+        })
+        .on("mouseout", () => d3.select("#tooltip").style("display", "none"));
 
     // Axes
     chart.append("g")
         .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(xScale).tickValues(xScale.domain().filter((d, i) => !(i % Math.floor(xScale.domain().length / 8))))) // Show fewer ticks
+        .call(d3.axisBottom(xScale).tickValues(xScale.domain().filter((d, i) => !(i % Math.floor(xScale.domain().length / 8))))) 
         .selectAll("text")
         .attr("transform", "rotate(-45)")
         .style("text-anchor", "end");
@@ -82,15 +103,17 @@ function renderHeatmap(data, binningInterval) {
     chart.append("g")
         .call(d3.axisLeft(yScale));
 
-    // Legend
+    // --- LEGEND BELOW CHART ---
+    const legendWidth = 300;
+    const legendHeight = 15;
+
+    const legendScale = d3.scaleLinear()
+        .domain([0, d3.max(binnedData, d => d[1].count)])
+        .range([0, legendWidth]);
+
     const legend = svg.append("g")
         .attr("class", "legend")
-        .attr("transform", `translate(${width - 100}, ${margin.top})`);
-
-    legend.append("text")
-        .text("Earthquake Frequency")
-        .attr("x", -20)
-        .attr("y", -10);
+        .attr("transform", `translate(${(width - legendWidth) / 2}, ${height + 100})`);
 
     const legendGradient = svg.append("defs")
         .append("linearGradient")
@@ -102,20 +125,25 @@ function renderHeatmap(data, binningInterval) {
     legendGradient.append("stop").attr("offset", "100%").attr("stop-color", d3.interpolateYlOrRd(1));
 
     legend.append("rect")
-        .attr("width", 100)
-        .attr("height", 10)
+        .attr("width", legendWidth)
+        .attr("height", legendHeight)
         .style("fill", "url(#legend-gradient)");
 
-    legend.append("text").text("Low").attr("x", -5).attr("y", 25);
-    legend.append("text").text("High").attr("x", 80).attr("y", 25);
+    legend.append("text").text("Low").attr("x", -20).attr("y", legendHeight / 2 + 5).style("font-size", "12px");
+    legend.append("text").text("High").attr("x", legendWidth + 5).attr("y", legendHeight / 2 + 5).style("font-size", "12px");
+
+    legend.append("g")
+        .attr("transform", `translate(0, ${legendHeight})`)
+        .call(d3.axisBottom(legendScale).ticks(5).tickFormat(d3.format(".0f")));
 }
 
 // Helper function to determine time bin format
 function getTimeFormat(interval) {
     switch (interval) {
         case 'day': return '%Y-%m-%d';
-        case 'week': return '%Y-%U';  // Week-based binning
-        case 'month': return '%Y-%m'; // Monthly binning
+        case 'week': return '%Y-%U';
+        case 'month': return '%Y-%m';
+        case 'year': return '%Y';
         default: return '%Y-%m';
     }
 }
